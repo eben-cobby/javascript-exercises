4c8937653ce7f8db27464eeda8fb2aaaf7e27b13
eben_cobby <104040859+eben-cobby@users.noreply.github.com>
2023-04-28 20:09:56 +0000

    solve exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let result = 0;
+  for (let item of array) {
+    result += item;
+  };
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let result = 1;
+  for (let item of array) {
+    result *= item;
+  };
+  return result;
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  return (number**power);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0) return 1;
+  result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result
+
 };
+console.log(multiply([2, 4]))
 
 // Do not edit below this line
 module.exports = {
