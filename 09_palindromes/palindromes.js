769f7dc1d1c84abd3725587d31d2d2455f6e764c
eben_cobby <104040859+eben-cobby@users.noreply.github.com>
2023-04-28 20:56:05 +0000

    solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.toLowerCase().replace(/[^a-z0-9]/g,"");
+    let reverseString = newString.split("").reverse().join("");
+    return newString === reverseString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
