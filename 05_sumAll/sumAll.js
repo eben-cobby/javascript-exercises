ea66a5946242e269e94660391a9d3116ba36c950
eben_cobby <104040859+eben-cobby@users.noreply.github.com>
2023-04-28 17:29:31 +0000

    solve 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+    if (firstNumber < 0 ||
+        secondNumber < -1 ||
+        !Number.isInteger(firstNumber) ||
+        !Number.isInteger(secondNumber)
+    ) {
+        return "ERROR";
+    }
 
+    let min = Math.min(firstNumber, secondNumber);
+    let max = Math.max(firstNumber, secondNumber);
+    let result = 0
+
+    for (let i = min; i <= max; i++) {
+        result += i;
+    };
+
+    return result;
 };
-
+console.log(sumAll(1, 5))
 // Do not edit below this line
 module.exports = sumAll;
