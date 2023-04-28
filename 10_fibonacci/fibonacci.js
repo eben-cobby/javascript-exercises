c33905e5700b2e43bb9b8680ffef642d228b178e
eben_cobby <104040859+eben-cobby@users.noreply.github.com>
2023-04-28 21:31:27 +0000

    Solve exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function (member) {
+    if (member < 0) return "OOPS";
+    if (member ===0) return 0;
+    if (member < 3) return 1;
+    return fibonacci(member - 1) + fibonacci(member - 2);
 };
-
+console.log(fibonacci(0));
 // Do not edit below this line
 module.exports = fibonacci;
