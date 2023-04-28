d6cce3e74cf76327b1ab6932e9065124dd7321ec
eben_cobby <104040859+eben-cobby@users.noreply.github.com>
2023-04-28 05:58:34 +0000

    solve exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let str = "";
+    for (num; num > 0; num--) {
+        str += string;
+    };
+    if (num < 0)
+        str += "ERROR"
+    return str;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
