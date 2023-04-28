715807b78aff5a51052178925e0987f1b1a15808
eben_cobby <104040859+eben-cobby@users.noreply.github.com>
2023-04-28 16:30:33 +0000

    solve 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
 
+const removeFromArray = function (array, ...valuesToRemove) {
+    // loop through value to remove
+    for (let valueToRemove of valuesToRemove) {
+        //if value to remove is in the array, remove it
+        let indexToRemove = array.indexOf(valueToRemove);
+        if (indexToRemove !== -1) {
+            array.splice(indexToRemove, 1);
+        };
+    };
+    return array;
 };
 
 // Do not edit below this line
