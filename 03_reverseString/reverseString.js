071c7ce784f7e42fd42e7704084216c1c0a84974
eben_cobby <104040859+eben-cobby@users.noreply.github.com>
2023-04-28 06:27:58 +0000

    solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let newString = string.split("").reverse().join("");
+    return newString;
 };
 
-// Do not edit below this line
 module.exports = reverseString;
