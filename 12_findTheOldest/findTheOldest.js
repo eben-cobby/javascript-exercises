4472369c397e785da75ff4784bc807c8e8a5067e
eben_cobby <104040859+eben-cobby@users.noreply.github.com>
2023-04-29 00:00:15 +0000

    Solve exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let mappedPeople = people.map(item => ({
+        name: item.name,
+        age: (item.yearOfDeath
+            ? item.yearOfDeath - item.yearOfBirth
+            : new Date().getFullYear() - item.yearOfBirth)
+    }));
 
+    let oldestPerson = mappedPeople.reduce((prev, current) => {
+        if (current.age > prev.age) {
+            return current;
+        } else {
+            return prev;
+        } 
+    });
+    return oldestPerson;
+    
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
